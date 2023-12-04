267660a2c113bba2956ca52dac393d27752dd00e
Yughie <yughiep@gmail.com>
2023-12-04 18:06:13 +0800

    Added error message

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Login from './components/Login.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Login />
   </React.StrictMode>,
 )
